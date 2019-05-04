d8730651b65003d31879a4c63c010d9180ab674a
giselireuwsaat <givasconcelo1@gmail.com>
2019-05-04 10:11:53 -0300

    Alterado footer, saiba mais e fale conosco responsive

--- versao_php/faleConosco.php
+++ versao_php/faleConosco.php
@@ -1,67 +1,75 @@
-        <section id="faleConosco" class="formulario">
-            <div class="container">
+<section class="fale-conosco">
+    <div class="container container-fc">
+        <h3>Escolha a melhor forma de falar com a gente:</h3>
+        <div id="panel" class="panel panel-default no-padding">
+            <div class="panel-body no-padding">
+                <form role="form" class="form-horizontal">
+                    <div class="row-fluid">
+                        <div class="col-md-3 col-sm-12 col-xs-12 banner">
+                            <!-- Background image -->
+                        </div>
+                        <div class="col-md-4 col-sm-6 col-xs-12 padding">
+                            <p class="lead">Rede Sociais</p>
 
-                <div class="row">
-                    <div class="col-lg-5">
-                        <h1>Encontre-nos pelas redes sociais</h1>
-                        <div class="icones">
-                            <a href="https://www.google.com.br/"> <img src="img/icones/facebook.svg" alt="facebook"></a>
-                            <a href="https://www.google.com.br/"><img src="img/icones/instagram.svg"
-                                    alt="instagram"></a>
-                            <a href="https://www.google.com.br/"><img src="img/icones/linkedin.svg" alt="linkedin"></a>
-                            <a href="https://www.google.com.br/"><img src="img/icones/twitter.svg" alt="twitter"></a>
+                            <div class="row">
+                                <div class="col-md-12 icones">
+                                    <a href="https://www.google.com.br/"> <img src="img/icones/facebook.svg"
+                                            alt="facebook"></a>
+                                
+                                    <a href="https://www.google.com.br/"><img src="img/icones/instagram.svg"
+                                            alt="instagram"></a>
+                                               
+                                    <a href="https://www.google.com.br/"><img src="img/icones/twitter.svg"
+                                            alt="twitter"></a>
+                                </div>
+
+                            </div>
+
+
+
+
                         </div>
                     </div>
 
+                    <div class="col-md-5 col-sm-12 col-xs-12  padding border-left">
+                        <p class="lead">Mensagem</p>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label label-default="" for="">Nome</label>
+                                <input type="text" name="nome" class="form-control">
+                            </div>
+                            <div class="col-md-6">
+                                <label label-default="" for="">Assunto</label>
+                                <input type="text" name="assunto" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label label-default="" for="">Email</label>
+                                <input type="email" name="email" class="form-control">
+                            </div>
+                            <div class="col-md-6">
+                                <label label-default="" for="">Telefone</label>
+                                <input type="text" name="telefone" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
 
-                    <div class="col-lg-2">
+                                <label label-default="" for="">Mensagem</label>
+                                <textarea name="mensagem" rows="3" style="resize: none" class="form-control"></textarea>
+
+                            </div>
+                        </div>
+                        <div class="row-fluid">
+                            <button class="btn btn-primary enviar">Enviar</button>
+                            <button class="btn btn-default limpar">Limpar</button>
+                        </div>
                     </div>
-
-
-                    <div class="col-lg-5">
-                        <form class="contato">
-                            <h1>Fale Conosco</h1>
-                            <br>
-
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                <input id="Nome" type="text" class="form-control" name="nome" placeholder="Nome">
-                            </div>
-
-                            <div class="form-group"></div>
-
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-                                <input id="Email" type="email" class="form-control" name="email" placeholder="Email">
-                            </div>
-
-                            <div class="form-group"></div> <!-- quebra linha -->
-
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-folder-close"></i></span>
-                                <input id="Assunto" type="text" class="form-control" name="assunto"
-                                    placeholder="Assunto">
-                            </div>
-
-                            <div class="form-group"></div>
-
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-pencil"></i></span>
-                                <textarea class="form-control" rows="3" placeholder="Mensagem"
-                                    style="resize: none"></textarea>
-                            </div>
-
-                            <input type="checkbox" id="enviarCopia" name="enviarCopia" value="enviarCopia">
-                            <label for="enviarCopia">Envie-me uma cópia do email</label>
-
-                            <div class="form-group"></div>
-
-                            <button type="button" class="btn btn-primary">Enviar</button>
-
-                        </form>
-                    </div>
-
-                </div>
             </div>
+            </form>
+        </div>
+    </div>
+</div>
+</section>
 
-        </section>
