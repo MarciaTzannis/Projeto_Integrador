6e4e5e5c54d8bf6197bacd7f60366db8db22d765
MarciaTzannis <marciatzannis@gmail.com>
2019-07-06 15:35:16 -0300

    Layout Pagina Cadastro

--- resources/views/cadastroUsuario.blade.php
+++ resources/views/cadastroUsuario.blade.php
@@ -4,7 +4,7 @@
 
 
 <div class="container" id="wrap">
-    
+
 <!-- Vericação de erros -->
 @if (count($errors) > 0)
     <div class="alert alert-danger">
@@ -14,146 +14,203 @@
             @endforeach
         </ul>
         @endif
-    </div> 
+    </div>
 
 
-    <div class="row" id="formulario">
-        <div class="col-md-6 col-md-offset-3">
-            <form method="POST" action="{{ route('register') }}" accept-charset="utf-8" class="form" role="form">
+
+
+
+<div class="container-contact100" id="formulario">
+		<div class="cadastroUsuario">
+			<form class="contact100-form validate-form"  action="{{ route('register') }}" method="post">
                 @csrf
-                <legend><b>Cadastre-se</b></legend>
-                <h4><em>Match Sports, o melhor jeito de encontrar seu parceiro.</em></h4>
+                <h3><legend><b>Cadastre-se</b></legend></h3>
+                <h4 class="contact100">
+                <b><em>Match Sports, encontre seu amigo no esporte.</em></b></h4>
+                <br>
 
-                <div class="form-row">
-
-                    <div class="form-group col-md-12">
-                        <label for="name">Nome Completo</label>
-                        <input type="text" name="name" class="form-control" id="name" placeholder="Name">
-                    </div>
-
-                    <div class="form-group col-md-12">
-                        <label for="inputEmail4">Email</label>
-                        <input type="email" name="email" class="form-control" id="inputEmail4" placeholder="Email">
-                    </div>
-
-                    <div class="form-group col-md-6">
-                        <label for="inputPassword4">Senha</label>
-                        <div class="input-group">
-                            <input type="password" name="password" class="form-control" id="inputPassword4"
-                                placeholder="Senha">
-                            <span class="input-group-btn">
-                                <button class="btn btn-default toggle-password" type="button">
-                                    <span class="glyphicon glyphicon-eye-close field-icon" aria-hidden="true"></span>
-                                </button>
-                            </span>
-                        </div>
-                    </div>
-
-                    <div class="form-group col-md-6">
-                        <label for="inputPassword4">Confirmar Senha</label>
-                        <input id="password-field-confirm" type="password" class="form-control"
-                            name="password_confirmation" value="secret">
-                    </div>
-
-                    <div class="form-group col-md-12">
-                        <label for="inputAddress">Endereço</label>
-                        <input type="text" name="endereco" class="form-control" id="inputAddress"
-                            placeholder="Avenida/Rua">
-                    </div>
-
-                    <div class="form-group col-md-5">
-                        <label for="inputCity">Cidade</label>
-                        <input type="text" name="cidade" class="form-control" id="inputCity">
-                    </div>
-
-                    <div class="form-group col-md-2">
-                        <label for="inputCEP">CEP</label>
-                        <input type="text" name="cep" class="form-control" id="inputCEP" maxlength="8">
-                    </div>
-
-                    <div class="form-group col-md-5">
-                        <label for="inputEstado">Estado</label>
-                        <select name="estado" id="estado">
-                            <option value="estado">Selecione o Estado</option>
-                            <option value="ac">Acre</option>
-                            <option value="al">Alagoas</option>
-                            <option value="am">Amazonas</option>
-                            <option value="ap">Amapá</option>
-                            <option value="ba">Bahia</option>
-                            <option value="ce">Ceará</option>
-                            <option value="df">Distrito Federal</option>
-                            <option value="es">Espírito Santo</option>
-                            <option value="go">Goiás</option>
-                            <option value="ma">Maranhão</option>
-                            <option value="mt">Mato Grosso</option>
-                            <option value="ms">Mato Grosso do Sul</option>
-                            <option value="mg">Minas Gerais</option>
-                            <option value="pa">Pará</option>
-                            <option value="pb">Paraíba</option>
-                            <option value="pr">Paraná</option>
-                            <option value="pe">Pernambuco</option>
-                            <option value="pi">Piauí</option>
-                            <option value="rj">Rio de Janeiro</option>
-                            <option value="rn">Rio Grande do Norte</option>
-                            <option value="ro">Rondônia</option>
-                            <option value="rs">Rio Grande do Sul</option>
-                            <option value="rr">Roraima</option>
-                            <option value="sc">Santa Catarina</option>
-                            <option value="se">Sergipe</option>
-                            <option value="sp">São Paulo</option>
-                            <option value="to">Tocantins</option>
-                        </select>
-                    </div>
+                <div class="wrap-input100 validate-input" data-validate="Nome obrigatório">
+                  <span class="label-input100" for="name">Nome Completo</span>
+                  <input class="input100" type="text" name="name" placeholder="Escreva seu nome" id="name">
+                  <span class="focus-input100"></span>
                 </div>
 
-                <div class="form-group col-md-12">
-                    <label>Data de Nascimento</label>
-                    <div class="row">
-                        <div class="form-group col-md-4">
-                            <input type="text" name="data_nascimento" id="datepicker" class="form-control"></<input>
+                <div class="wrap-input100" data-validate="Email obrigatório: ex@abc.xyz">
+					<span class="label-input100" for="inputEmail4" >Email</span>
+					<input class="input100 @error('email') is-invalid @enderror" id="inputEmail4" type="email" name="email" placeholder="Escreva seu email" autocomplete="email">
+                    <span class="focus-input100"></span>
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+				</div>
 
-                        </div>
-                        <label>Sexo : </label> <label class="radio-inline">
-                            <input type="radio" name="sexo" value="M" id=male /> Male
-                        </label>
-                        <label class="radio-inline">
-                            <input type="radio" name="sexo" value="F" id=female /> Female
-                        </label>
-                    </div>
-                </div>
+				<div class="wrap-input50" data-validate="Senha obrigatória: Mín 8 caracteres.">
 
-                <div class="form-group col-md-12">
-                </div>
+					<span class="label-input100" for="inputPassword4">Senha</span>
+					<input class="input100 @error('password') is-invalid @enderror" id="inputPassword4" type="password" name="password" placeholder="Digite sua senha" >
+          <!-- <span class="input-group-btn">
+              <button class="btn toggle-password" type="button">
+                  <span class="glyphicon glyphicon-eye-close field-icon" aria-hidden="true"></span>
+              </button>
+          </span> -->
+                    <span class="focus-input100"></span>
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+				</div>
 
-                <div class="form-group col-md-12">
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="gridCheck">
-                        <label class="form-check-label" for="gridCheck">Eu aceito os termos e condições.</label>
-                    </div>
-                </div>
-
-                <div class="form-group col-md-6">
-                    <a href="{{ url('/auth/facebook')}}" class="btn btn-fb btn-block"><i
-                            class="fa fa-facebook"></i>Cadastre com Facebook</a>
-                </div>
-
-                <div class="form-group col-md-6">
-                    <a href="{{ url('/auth/google')}}" class="btn btn-tw btn-block"><i
-                            class="fa fa-google+"></i>Cadastre com Google+</a>
-
-                </div>
-
-                <button class="btn btn-lg btn-primary btn-block signup-btn" type="submit">
-                    Criar minha conta</button>
+        <div class="wrap-input50" data-validate="Precisa ser igual asenha.">
+          <span class="label-input100" for="inputPassword4">Confirmar Senha</span>
+          <input class="input100 @error('password') is-invalid @enderror" id="password-field-confirm" type="password" name="password_confirmation" placeholder="Confirme a senha" >
+                            <span class="focus-input100"></span>
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
         </div>
-    </form>
-    
-</div>
-</div>
 
 
-</div>
-<br>
+                <div class="wrap-input100 validate-input" data-validate="Coloque um endereço valido.">
+                  <span class="label-input100" for="inputAddress">Endereço</span>
+                  <input class="input100" type="text" name="endereco" placeholder="Avenida/Rua" id="inputAddress">
+                  <span class="focus-input100"></span>
+                </div>
+
+
+                <div class="wrap-input50 validate-input" data-validate="Coloque uma cidade valida.">
+                  <span class="label-input100" for="inputCity">Cidade</span>
+                  <input class="input100" type="text" name="cidade" id="inputCity">
+                  <span class="focus-input100"></span>
+                </div>
+
+                <div class="wrap-input50 validate-input" data-validate="Selecione um Estado">
+                  <span class="label-input100" for="inputEstado">Estado</span>
+                  <select class="input100" type="text" name="estado" id="inputEstado">
+                    <option value="estado">Selecione o Estado</option>
+                    <option value="ac">Acre</option>
+                    <option value="al">Alagoas</option>
+                    <option value="am">Amazonas</option>
+                    <option value="ap">Amapá</option>
+                    <option value="ba">Bahia</option>
+                    <option value="ce">Ceará</option>
+                    <option value="df">Distrito Federal</option>
+                    <option value="es">Espírito Santo</option>
+                    <option value="go">Goiás</option>
+                    <option value="ma">Maranhão</option>
+                    <option value="mt">Mato Grosso</option>
+                    <option value="ms">Mato Grosso do Sul</option>
+                    <option value="mg">Minas Gerais</option>
+                    <option value="pa">Pará</option>
+                    <option value="pb">Paraíba</option>
+                    <option value="pr">Paraná</option>
+                    <option value="pe">Pernambuco</option>
+                    <option value="pi">Piauí</option>
+                    <option value="rj">Rio de Janeiro</option>
+                    <option value="rn">Rio Grande do Norte</option>
+                    <option value="ro">Rondônia</option>
+                    <option value="rs">Rio Grande do Sul</option>
+                    <option value="rr">Roraima</option>
+                    <option value="sc">Santa Catarina</option>
+                    <option value="se">Sergipe</option>
+                    <option value="sp">São Paulo</option>
+                    <option value="to">Tocantins</option>
+                  </select>
+
+                </div>
+
+                <div class="wrap-input50 validate-input" data-validate="Coloque um CEP valido: xxxxx-xxx">
+                  <span class="label-input100" for="inputCEP">CEP</span>
+                  <input class="input100" type="text" name="cep" id="inputCEP">
+                  <span class="focus-input100"></span>
+                </div>
+
+                <div class="wrap-input50 validate-input" data-validate="Coloque uma cidade valida.">
+                  <span class="label-input100" for="inputCity">Data de Nascimento</span>
+                  <input class="input100" type="text" name="data_nascimento" id="datepicker">
+                  <span class="focus-input100"></span>
+                </div>
+
+
+                <div class="wrap-input100 validate-input" data-validate="Coloque uma cidade valida.">
+                  <span class="label-input100" for="inputCity">Sexo</span>
+                  <div class="input100">
+
+                    <input class="radio-inline" type="radio" name="sexo" value="M" id="male"> Masculino
+                    <input class="radio-inline" type="radio" name="sexo" value="F" id="female"> Feminino
+                  </div>
+
+                </div>
+
+
+                <span class="label-input100" for="inputCity">Login via:</span>
+
+                <div class="wrap-input100 social-buttons">
+                    <a href="{{ url('/auth/facebook')}}" class="btn btn-fb"><i class="fa fa-facebook"></i>
+                        Facebook</a>
+                    <a href="{{ url('/auth/google')}}" class="btn btn-tw"><i class="fa fa-google+"></i>
+                        Google+</a>
+                </div>
+
+
+				<div class="container-contact100-form-btn">
+					<div class="wrap-contact100-form-btn">
+						<div class="contact100-form-bgbtn"></div>
+						<button class="contact100-form-btn" type="submit">
+							<span>
+                            {{ __('Criar minha conta') }}
+								<i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
+							</span>
+						</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+
+    <div id="dropDownSelect1"></div>
+
+    <!--===============================================================================================-->
+    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/animsition/js/animsition.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/bootstrap/js/popper.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <script>
+        $(".selection-2").select2({
+            minimumResultsForSearch: 20,
+            dropdownParent: $('#dropDownSelect1')
+        });
+    </script>
+    <!--===============================================================================================-->
+    <script src="vendor/daterangepicker/moment.min.js"></script>
+    <script src="vendor/daterangepicker/daterangepicker.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/countdowntime/countdowntime.js"></script>
+    <!--===============================================================================================-->
+    <script src="js/main.js"></script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-23581568-13');
+    </script>
+
+
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
   <script>
@@ -161,6 +218,7 @@
     $( "#datepicker" ).datepicker({
       changeMonth: true,
       changeYear: true,
+      yearRange: '-90:-16',
       dateFormat: 'dd-mm-yy',
         dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado','Domingo'],
         dayNamesMin: ['D','S','T','Q','Q','S','S','D'],
@@ -168,7 +226,7 @@
         monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
         monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez']
     });
-    
+
   } );
   </script>
 
